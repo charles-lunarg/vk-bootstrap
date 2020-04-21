5e5b446d41eb0a387a20e42f3f7616325747f007
Charles Giessen <charles@lunarg.com>
2020-04-21 12:14:25 -0600

    Renamed swapchain error for missing surface.
    Now its 'surface_handle_not_provided' instead of not_present.
    More clear because you have to manually provide a surface handle, its
    not implicitely created for you.

--- src/VkBootstrap.h
+++ src/VkBootstrap.h
@@ -125,7 +125,7 @@ enum class DeviceError {
 	failed_create_device,
 };
 enum class SwapchainError {
-	surface_handle_not_present,
+	surface_handle_not_provided,
 	failed_query_surface_support_details,
 	failed_create_swapchain,
 	failed_get_swapchain_images,
@@ -524,7 +524,7 @@ void destroy_swapchain (Swapchain const& swapchain);
 class SwapchainBuilder {
 	public:
 	SwapchainBuilder (Device const& device);
-	SwapchainBuilder (Device& device, VkSurfaceKHR const surface);
+	SwapchainBuilder (Device const& device, VkSurfaceKHR const surface);
 	SwapchainBuilder (VkPhysicalDevice const physical_device, VkDevice const device, VkSurfaceKHR const surface);
 
 	detail::Expected<Swapchain, detail::Error<SwapchainError>> build () const;

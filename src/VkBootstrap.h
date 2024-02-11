a3302276665041b9cc968d50d4e84679985bb203
Charles Giessen <charles@lunarg.com>
2024-02-11 12:48:55 -0600

    Use vulkan/vulkan_core.h instead of vulkan/vulkan.h
    vulkan.h includes platform specific headers that cause non trivial build
    overhead. Because vk-bootstrap doesn't make use of any platform specific
    API's, it can move over to vulkan_core.h.
    
    This *MAY* break users of the library who were depending on vulkan/vulkan.h
    including other headers, like windows.h. But because the benefits outweigh
    the drawbacks, it is a good change.

--- src/VkBootstrap.h
+++ src/VkBootstrap.h
@@ -24,7 +24,7 @@
 #include <string>
 #include <system_error>
 
-#include <vulkan/vulkan.h>
+#include <vulkan/vulkan_core.h>
 
 #include "VkBootstrapDispatch.h"
 
--- src/VkBootstrapDispatch.h
+++ src/VkBootstrapDispatch.h
@@ -21,7 +21,7 @@
 
 #pragma once
 
-#include <vulkan/vulkan.h>
+#include <vulkan/vulkan_core.h>
 
 namespace vkb {
 

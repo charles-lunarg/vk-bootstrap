8a941c5fe80f35a215f2ecc1e9600a05a41bcca9
Charles Giessen <charles@lunarg.com>
2025-02-17 13:52:12 -0600

    Add 1.3 and 1.4 version error codes.
    Forgot to do that during the 1.3 & 1.4 release...

--- src/VkBootstrap.h
+++ src/VkBootstrap.h
@@ -211,6 +211,8 @@ enum class InstanceError {
     vulkan_version_unavailable,
     vulkan_version_1_1_unavailable,
     vulkan_version_1_2_unavailable,
+    vulkan_version_1_3_unavailable,
+    vulkan_version_1_4_unavailable,
     failed_create_instance,
     failed_create_debug_messenger,
     requested_layers_not_present,

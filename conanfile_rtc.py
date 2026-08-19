b35d3b100ec6847745f457abdb9e687dd4f244d7
julialubiszewska <julialubiszewska12@gmail.com>
2026-08-19 14:28:47 +0100

    Update version to 1.4.360

--- conanfile_rtc.py
+++ conanfile_rtc.py
@@ -3,7 +3,7 @@ from conans import ConanFile
 
 class VkBootstrapConan(ConanFile):
     name = "vk-bootstrap"
-    version = "1.4.353"
+    version = "1.4.360"
     url = "https://github.com/Esri/vk-bootstrap/blob/runtimecore/"
     license = "https://github.com/Esri/vk-bootstrap/blob/runtimecore/LICENSE.md"
     description = "A utility library that jump starts initialization of Vulkan"
